f583db5499ad7759ee944ac4a64950db9fcb9c22
tjennyyy <jenny_tang@gmx.net>
2019-05-15 10:51:40 +0200

    add

--- src/Employees.java
+++ src/Employees.java
@@ -1,3 +1,48 @@
 public class Employees {
 
+    private String name;
+    private String surname;
+    private int yearOfBirth;
+    private int salary;
+
+    public Employees (String name, String surname, int yearOfBirth, int salary){
+        this.name = name;
+        this.surname = surname;
+        this.yearOfBirth = yearOfBirth;
+        this.salary = salary;
+    }
+
+    public String getName (){
+        return name;
+    }
+
+    public void setName (String newName) {
+        name = newName;
+    }
+
+    public String getSurname(){
+        return surname;
+    }
+
+    public void setSurname(String newSurname){
+        surname = newSurname;
+    }
+
+    public int getYearOfBirth(){
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int newYearOfBirth){
+        yearOfBirth = newYearOfBirth;
+    }
+
+    public int getSalary(){
+        return salary;
+    }
+
+    public void setSalary(int newSalary){
+        salary = newSalary;
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("");
+
+        Employees e1 = new Employees("John", "Doe", 1980, 2500);
+        Employees e2 = new Employees("Joanna", "Doe", 1982, 2200);
+        System.out.println("Name: " + e1.getName() + " " + "Surname: " + e1.getSurname() + ", " + e1.getYearOfBirth() + ", " + e1.getSalary()+" EUR");
+        System.out.println("Name: " + e2.getName() + " " + "Surname: " + e2.getSurname() + ", " + e2.getYearOfBirth() + ", " + e2.getSalary()+" EUR");
+
+
     }
 }
