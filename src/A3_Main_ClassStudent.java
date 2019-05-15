cb24f0b7a1e0d9a987da01c7475096067e85bc94
tjennyyy <jenny_tang@gmx.net>
2019-05-15 13:36:44 +0200

    adding

---
+++ src/A3_Main_ClassStudent.java
@@ -0,0 +1,32 @@
+public class A3_Main_ClassStudent {
+
+    public static void main(String[] args) {
+
+        ClassStudent clS1 = new ClassStudent("John", "Doe", 2005, "3b");
+        ClassStudent clS2 = new ClassStudent("Joanna", "Doe", 2005, "3a");
+        ClassStudent clS3 = new ClassStudent("Mario", "Smith", 2004, "4a");
+        ClassStudent clS4 = new ClassStudent("Maria", "Smith", 2004, "4a");
+        ClassStudent clS5 = new ClassStudent("Lisa", "Tal", 2003, "2a");
+       // System.out.println(clS1.getFirstname() + " " + clS1.getLastname() + "" + " is born " + clS1.getYearOfBirth() + " is now " + clS1.getAge() + " old"+ " and goes to class " + clS1.getClassNumber());
+
+
+        ClassStudent[] students = {clS1, clS2, clS3, clS4, clS5};
+
+        students[0].printData();
+
+
+        for (int i = 0; i < students.length; i++){
+            students[i].printData();
+
+        }
+
+    }
+}
+
+// String[] arrayStudent = new String[5];
+//        arrayStudent[0] = "John, Doe, 2005, 3b";
+//        arrayStudent[1] = "Joanna, Doe, 2005, 3a";
+//        arrayStudent[2] = "Mario, Smith, 2006, 4b";
+//        arrayStudent[3] = "Maria, Smith, 2006, 4a";
+//        arrayStudent[4] = "Lisa, Tal, 2004, 2b";
+//        arrayStudent[5] = "Louis, Tal, 2004, 2a";
---
+++ src/ClassStudent.java
@@ -0,0 +1,64 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class ClassStudent {
+
+    private String firstname;
+    private String lastname;
+    private int yearOfBirth;
+    private int age;
+    private String classNumber;
+
+    public ClassStudent(String firstname, String lastname, int yearOfBirth, String classNumber){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.yearOfBirth = yearOfBirth;
+        GregorianCalendar actYear = (GregorianCalendar) Calendar.getInstance();
+        this.age = (actYear.get(Calendar.YEAR) - yearOfBirth);
+        this.classNumber = classNumber;
+    }
+
+    public String getFirstname(){
+        return firstname;
+    }
+
+    public void setFirstname(String newFirstname){
+        firstname = newFirstname;
+    }
+
+    public String getLastname(){
+        return lastname;
+    }
+
+    public void setLastname(String newLastName){
+        lastname = newLastName;
+    }
+
+    public int getYearOfBirth(){
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int newYearOfBirth){
+        yearOfBirth = newYearOfBirth;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int newAge){
+        age = newAge;
+    }
+
+    public String getClassNumber(){
+        return classNumber;
+    }
+
+    public void setClassNumber(String newClassNumber){
+        classNumber = newClassNumber;
+    }
+
+    public void printData (){
+        System.out.println(this.firstname + " " + this.lastname + ", born on " +  this.yearOfBirth + ", age " + this.age + ", goes to class " + this.classNumber);
+    }
+}
